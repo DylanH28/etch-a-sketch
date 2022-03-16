842daf64cfa959845773c70854b63dba97f8b035
Dylan <dylanhill1228@gmail.com>
2022-03-16 16:27:05 -0500

    Completed project and fixed javascript functions and listeners.

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
 const grid = document.querySelector('.grid');
-const reset = document.querySelector('.reset');
+const resetButton = document.querySelector('.reset');
 let gridSize = document.querySelector('.gridSize');
 let gridInput = document.querySelector('input');
+const applyGridSize = document.querySelector('.apply');
 
+//Creates default grid
 let boxSize = 30;
 createGrid(boxSize);
 
+//Creating divs using javascript
 function createDiv(size) {
     const div = document.createElement('div');
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
-    div.style.border = '1px thin black';
+    div.classList.add('box');
     return div;
 }
 
+//Creates grid using the provided grid size
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
           grid.appendChild(createDiv(grid.clientWidth / gridSize));
         }
-      }
+    }
 }
 
-gridInput.addEventListener('input', function (e) {
-    squareSize = e.target.value;
-    gridSize.textContent = `${squareSize}x${squareSize}`;
+//Refreshes page
+function reset() {
+    location.reload();
+}
+
+//Uses slider input to change grid size
+
+
+//Mouse hovering targets childs in the grid. Then adds an active class to those targeted childs.
+grid.addEventListener('mouseover', function (e) {
+  if (e.target.matches('.box')) {
+    e.target.classList.add('active');
+  }
 });
+
+gridInput.addEventListener('input', function (e) {
+    boxSize = e.target.value;
+    gridSize.textContent = `${boxSize}x${boxSize}`;
+});
+
+applyGridSize.addEventListener('click', function () {
+    while(grid.firstChild){
+        grid.removeChild(grid.firstChild);
+    }
+    createGrid(boxSize);
+});
+
+//Calls reset function when reset button is clicked.
+resetButton.addEventListener('click', reset)
